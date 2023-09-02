26e1cd7545b4b4a157e62e8801040517d738d54b
sadbatya <sadbatya@mail.ru>
2023-09-02 12:25:27 +0200

    feat context menu

--- src/app.js
+++ src/app.js
@@ -1,2 +1,20 @@
 import './styles.css'
 
+import { ContextMenu } from './menu';
+import { TestModule } from './modules/test.module';
+
+const contextMenu = new ContextMenu('#menu');
+
+const addTestModule = new TestModule('add_text_block', 'Добваить тестовый блок с текстом');
+
+contextMenu.add(addTestModule);
+
+addTestModule.trigger = () => {
+    const newParagraph = document.createElement('p');
+    newParagraph.textContent = 'тестовый текст';
+    document.body.append(newParagraph);
+}
+
+document.body.addEventListener('contextmenu', (event) => {
+    contextMenu.showContextMenu(event);
+});
--- src/menu.js
+++ src/menu.js
@@ -1,5 +1,35 @@
-import {Menu} from './core/menu'
+import { Menu } from './core/menu'
 
 export class ContextMenu extends Menu {
+    constructor(selector) {
+        super(selector);
+        this.menuItems = [];
+        this.menu = null;
+        this.createMenu();
+    }
 
+    createMenu() {
+        this.menu = document.querySelector('#menu');
+    }
+
+    open() {
+        this.menu.classList.add('open');
+    }
+
+    close() {
+        this.menu.classList.remove('open');
+    }
+
+    add(module) {
+        this.menuItems.push(module);
+        const menuItem = module.toHTML();
+        this.menu.append(menuItem)
+    }
+
+    showContextMenu(event) {
+        event.preventDefault();
+        this.open();
+        this.menu.style.left = `${event.clientX}px`;
+        this.menu.style.top = `${event.clientY}px`
+    }
 }
